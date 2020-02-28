c62d2438aa7261bfb7b04eb264307aa3f84d0707
chopo156 <eduardojoan@gmail.com>
2020-02-28 12:25:52 -0300

    variables are part of the script

--- firescript/__resource.lua
+++ firescript/__resource.lua
@@ -2,11 +2,13 @@ resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 
 server_scripts 
 {
-	"server.lua"
+	"server.lua",
+	"vars/variables.lua",
 }
 client_scripts 
 {
-	"client.lua"
+	"client.lua",
+	"vars/variables.lua",
 }
 
 -- Export function
